df55b3270907a813462e70e3aa970705a848f31f
Agnieszka <agnieszka.iga.nowacka@gmail.com>
2024-07-01 23:54:01 +0200

    "dod"

--- skrypt.sh
+++ skrypt.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-if [[ "$1" == "--date" ]]; then
+if [[ "$1" == "--date" ]] || [[ "$1" == "-d" ]]; then
 echo "dzisiejsza data: $(date)"
 fi
 echo "ok"
-if  [[ "$1" == "--logs" ]]; then
+if  [[ "$1" == "--logs" ]] || [ "$1" == "-l" ]]; then
 for i in {1..100}; do
 echo "numer pliku: $i, utworzony przez skrypt.sh, $(date)" > log_$i.txt
 done
 fi
-if  [[ "$1" == "--logs[0-9]" ]]; then
+if  [[ "$1" == "--logs[0-9]" ]] || [[ "$1" == "-l[0-9" ]]; then
 for i in $(seq 1 "$1"); do
 echo "numer pliku: $i utworzony przez skrypt.sh dnia $(date)"
 done
@@ -17,6 +17,6 @@ echo "flaga nieznana"
 fi
 if [[ "$1" == "--help" ]]; then
 echo "opcje"
-echo " --date wyświetla dzisiejszą datę"
-echo "--logs tworzy 100 plików log_X.txt gdzie X to numer pliku"
-echo "--logsX tworzy X plików j/w "
+echo " --date i -d wyświetla dzisiejszą datę"
+echo "--logs - -l tworzy 100 plików log_X.txt gdzie X to numer pliku"
+echo "--logsX i -lX tworzy X plików j/w "
